89a6f05fd794c64d8a764d2a8a19b977994fd865
George Polevoy <george.polevoy@gmail.com>
2015-02-07 00:11:25 +0300

    improved the perf test

--- JumboExcel.AdHocTests/ExploreLimitsTests.cs
+++ JumboExcel.AdHocTests/ExploreLimitsTests.cs
@@ -10,20 +10,53 @@ using NUnit.Framework;
 
 namespace JumboExcel
 {
+    /// <summary>
+    /// Explores performance for various excel sheet widths and heights. Reports the results in a beautiful logarithmic scale diagram.
+    /// </summary>
     class ExploreLimitsTests
     {
-        private const int Timeout = 1000;
-        private const int MaxCells = 500000;
-        private const int MinCells = 4000;
-        private const int MaxRows = 4000;
-        private const int MaxColumns = 4000;
-        private const int ScaleDivisor = 100;
+        // Can try smaller constance for debugging.
+        //private const int Timeout = 100;
+        //private const int MaxCells = 200000;
+        //private const int MinCells = 80;
+        //private const int MaxRows = 8000;
+        //private const int MaxColumns = 8000;
+        //private const int ScaleDivisor = 10;
 
+        /// <summary>
+        /// Limit the execution time. We are interested only in times belo 2000. Some results will exceed this limit, but the exploration would stop at that point.
+        /// </summary>
+        private const int Timeout = 2000;
+
+        /// <summary>
+        /// We are aiming at generating a meg of cells in specified timeout.
+        /// </summary>
+        private const int MaxCells = 1000000;
+        /// <summary>
+        /// Not interested in the results belo.
+        /// </summary>
+        private const int MinCells = 1000;
+
+        private const int MaxRows = 1000000;
+        private const int MaxColumns = 1000000;
+
+        /// <summary>
+        /// Header style for performance diagram.
+        /// </summary>
+        readonly NumberStyle perfHeaderStyle = new NumberStyle(null, null, Border.NONE, Color.Wheat);
+
+        /// <summary>
+        /// Visually exploring performance. Exercises various worksheet widths and heights.
+        /// Runs for a long time, reporting it's progress in console output, than generates a nice Excel diagram and uses shell execute to open it in Excel.
+        /// </summary>
+        /// <remarks>
+        /// Digram would be a very large hyperbolic shape, so using logarithmic scale for width and height.
+        /// </remarks>
         [Test, Explicit]
-        public void ExploreFileSize()
+        public void ExploreWorksheetSizeGenerationPerformance()
         {
             var timings = new Dictionary<Tuple<int,int>, long>();
-            foreach (var h in ExploreUpperBound(h=> CanGenerateHeight(h + 1, timings)))
+            foreach (var h in ExploreUpperBound(h=> CanGenerateHeight(h, timings), 1))
             {
                 Console.WriteLine("Height {0} is ok.", h);
             }
@@ -54,19 +87,9 @@ namespace JumboExcel
             TestHelper.WriteAndExecuteExcel(new[]
             {
                 new Worksheet("Timings", new WorksheetParametersElement(),
-                    GetSummaryRows(timingsDictionary, rowHeadersDictionary, columnHeadersDictionary))
+                    GetPerfSummaryRows(timingsDictionary, rowHeadersDictionary, columnHeadersDictionary))
             });
         }
-
-        T? GetFromDictionary<T>(Dictionary<int, T> d, int index) where T : struct
-        {
-            T element;
-            if (d.TryGetValue(index, out element))
-                return element;
-            return null;
-        }
-
-        readonly NumberStyle headerStyle = new NumberStyle(null, null, Border.NONE, Color.Wheat);
 
         static NumberStyle CreateNumberStyle(double intensity)
         {
@@ -74,45 +97,45 @@ namespace JumboExcel
             return new NumberStyle(null, null, Border.NONE, Color.FromArgb(128 + component / 2, 128 + component / 2, 255));
         }
 
-        CellElement CreateCell(double? value, NumberStyle style)
+        CellElement CreatePerfHeaderCell(double? value, NumberStyle style)
         {
             return new DecimalCell(value.HasValue ? (decimal?)value.Value : null, style);
         }
 
-        private IEnumerable<Row> GetSummaryRows(Dictionary<Tuple<int, int>, double> timingsDictionary, Dictionary<int, double> rowHeadersDictionary, Dictionary<int, double> columnHeadersDictionary)
+        private IEnumerable<Row> GetPerfSummaryRows(Dictionary<Tuple<int, int>, double> timingsDictionary, Dictionary<int, double> rowHeadersDictionary, Dictionary<int, double> columnHeadersDictionary)
         {
             var cellElements = new List<CellElement>{EmptyCell.Instance};
-            cellElements.AddRange(Enumerable.Range(1, Scale(MaxColumns)).Select(i => CreateCell(GetFromDictionary(rowHeadersDictionary, i), headerStyle)));
+            cellElements.AddRange(Enumerable.Range(1, Scale(MaxColumns)).Select(i => CreatePerfHeaderCell(TryGet(rowHeadersDictionary, i), perfHeaderStyle)));
             yield return new Row(cellElements);
 
             foreach (var y in Enumerable.Range(1, Scale(MaxRows)))
             {
-                yield return CreateRow(timingsDictionary, y, columnHeadersDictionary);
+                yield return CreatePerfRow(timingsDictionary, y, columnHeadersDictionary);
             }
         }
 
-        private Row CreateRow(Dictionary<Tuple<int, int>, double> timingsDictionary, int y, Dictionary<int, double> columnHeadersDictionary)
+        private Row CreatePerfRow(Dictionary<Tuple<int, int>, double> timingsDictionary, int y, Dictionary<int, double> columnHeadersDictionary)
         {
-            var cellElements = new List<CellElement> { CreateCell(GetFromDictionary(columnHeadersDictionary, y), headerStyle) };
-            cellElements.AddRange(Enumerable.Range(1, Scale(MaxColumns)).Select(x => Tuple.Create(x, y)).Select(cell => timingsDictionary.ContainsKey(cell) ? (CellElement)CreateValueCell((decimal)timingsDictionary[cell]) : EmptyCell.Instance));
+            var cellElements = new List<CellElement> { CreatePerfHeaderCell(TryGet(columnHeadersDictionary, y), perfHeaderStyle) };
+            cellElements.AddRange(Enumerable.Range(1, Scale(MaxColumns)).Select(x => Tuple.Create(x, y)).Select(cell => timingsDictionary.ContainsKey(cell) ? (CellElement)CreatePerfValueCell((decimal)timingsDictionary[cell]) : EmptyCell.Instance));
             return new Row(cellElements);
         }
 
-        private static DecimalCell CreateValueCell(decimal number)
+        private static DecimalCell CreatePerfValueCell(decimal number)
         {
             return new DecimalCell(number, CreateNumberStyle((double)number / Timeout));
         }
 
         int Scale(long dimension)
         {
-            //return (int) Math.Log(dimension, 2);
-            return (int)(dimension / ScaleDivisor);
+            return (int) Math.Log(dimension, 2);
+            //return (int)(dimension / ScaleDivisor);
         }
 
         private bool CanGenerateHeight(long h, Dictionary<Tuple<int,int>, long> timings)
         {
-            var exploreUpperBound = ExploreUpperBound(w => CanGenerateWidth(w + 1, h, timings)).ToList();
-            return exploreUpperBound.Count > 1;
+            var exploreUpperBound = ExploreUpperBound(w => CanGenerateWidth(w, h, timings), 1).ToList();
+            return exploreUpperBound.Count > 0;
         }
 
         private bool CanGenerateWidth(long w, long h, Dictionary<Tuple<int, int>, long> timings)
@@ -155,42 +178,56 @@ namespace JumboExcel
         }
 
         [Test, Explicit]
-        [TestCase(0)]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(10)]
-        [TestCase(63)]
-        [TestCase(64)]
-        [TestCase(65)]
-        [TestCase(100)]
-        [TestCase(1000000)]
-        public void ExploreUpperBound(long bound)
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(0, 2)]
+        [TestCase(1, 2)]
+        [TestCase(0, 3)]
+        [TestCase(0, 10)]
+        [TestCase(0, 63)]
+        [TestCase(1, 63)]
+        [TestCase(2, 63)]
+        [TestCase(0, 64)]
+        [TestCase(0, 65)]
+        [TestCase(0, 100)]
+        [TestCase(0, 1000000)]
+        public void ExploreUpperBoundTest(long first, long last)
         {
-            var exploreUpperBound = ExploreUpperBound(i=>i<=bound).ToList();
-            Assert.AreEqual(bound, exploreUpperBound.Last());
-            Assert.LessOrEqual(exploreUpperBound.Count, 2 * Math.Log(bound + 1, 2) + 1);
+            var possibleValuesFound = ExploreUpperBound(i=>i<=last, first).ToList();
+            var allPossibleValues = new HashSet<long>(Enumerable.Range((int) first, (int) (last - first + 1)).Select(i => (long) i));
+            CollectionAssert.AllItemsAreUnique(possibleValuesFound);
+            CollectionAssert.IsSubsetOf(possibleValuesFound, allPossibleValues);
+            CollectionAssert.AreEqual(possibleValuesFound.OrderBy(i => i), possibleValuesFound);
+            Assert.AreEqual(last, possibleValuesFound.Last());
+            Assert.LessOrEqual(possibleValuesFound.Count, 2 * Math.Log(last + 1, 2) + 1);
         }
 
-        static IEnumerable<long> ExploreUpperBound(Func<long, bool> check)
+        /// <summary>
+        /// Explores the boolean function to find the upper index, at which it returns false.
+        /// </summary>
+        /// <param name="check">Boolean function to explore.</param>
+        /// <param name="first">Lower index.</param>
+        /// <returns>Returns a sequence of successful explored values in ascending order.</returns>
+        static IEnumerable<long> ExploreUpperBound(Func<long, bool> check, long first)
         {
-            if (!check(0))
+            if (!check(first))
                 yield break;
-            yield return 0;
+            yield return first;
             var current = 1;
-            while(current < long.MaxValue)
+            while(current + first < long.MaxValue / 2)
             {
-                if (check(current))
-                    yield return current;
+                if (check(current + first))
+                    yield return current + first;
                 else
                 {
                     current /= 2;
                     var convergeStep = current / 2;
                     while (convergeStep > 0)
                     {
-                        if (check(current + convergeStep))
+                        if (check(current + convergeStep + first))
                         {
-                            yield return current + convergeStep;
+                            yield return current + convergeStep + first;
                             current += convergeStep;
                         }
                         convergeStep /= 2;
@@ -214,7 +251,7 @@ namespace JumboExcel
                 Console.WriteLine(testSize);
             }
         }
-        
+
         static IEnumerable<int> GetTestSizes()
         {
             yield return 1;
@@ -223,6 +260,14 @@ namespace JumboExcel
                 yield return 1 << i;
                 yield return (1 << i) + (1 << (i - 1));
             }
+        }
+
+        static T? TryGet<T>(IDictionary<int, T> d, int index) where T : struct
+        {
+            T element;
+            if (d.TryGetValue(index, out element))
+                return element;
+            return null;
         }
     }
 }
